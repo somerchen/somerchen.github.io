bd7b77b976a6727adadc4a799f47ea2500c5c08a
somerchen <somercxb@gmail.com>
2022-01-03 17:57:47 +0800

    feat: set docs path

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from "vuepress/config";
 export default defineConfig({
   title: 'cxb',
   description: 'this is cxb site',
-  // dest: './',
+  base: '/document/',
+  dest: './document',
 });
