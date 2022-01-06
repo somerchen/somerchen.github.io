d02029ce0dd1184a8bf4a4012777c9373afeef11
somerchen <somercxb@gmail.com>
2022-01-06 21:12:28 +0800

    remove vuepress

--- docs/.vuepress/config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vuepress/config";
-
-export default defineConfig({
-  title: 'cxb',
-  description: 'this is cxb site',
-  base: '/document/',
-  dest: './document',
-});
